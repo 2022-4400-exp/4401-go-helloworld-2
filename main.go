b45e3f385cc0c4575e255e153fc8e47eaf44c870
waterandbiscuit <m19004@g.metro-cit.ac.jp>
2023-01-10 13:36:44 +0900

    change message

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4401", "message": "こんばんぱ"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4401", "message": "こんにちぱ"})
     }
 }
